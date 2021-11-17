30c7b526fc7f60d8e158200482412eafff199b65
Tobias Hanf <mail@tobias-hanf.de>
2021-11-17 11:36:47 +0100

    Add exercises directory

---
+++ exercises/01_Temperatur/Temperatur.java
@@ -0,0 +1,15 @@
+public class Temperatur {
+
+    public static void main(String[] args) {
+        float c20 = 20f;
+        float c30 = 30f;
+        float c40 = 40f;
+        float c120 = 120f;
+
+        System.out.println(c20 * 9 / 5 + 32);
+        System.out.println(c30 * 9 / 5 + 32);
+        System.out.println(c40 * 9 / 5 + 32);
+        System.out.println(c120 * 9 / 5 + 32);
+    }
+
+}
---
+++ exercises/03_Calculate/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculate calc = new Calculate(1, 2, "+");
+        System.out.println(calc.toString());
+
+        calc.setA(3);
+        System.out.println(calc.toString());
+
+        calc.setA(6);
+        calc.setOperation("/");
+        System.out.println(calc.toString());
+
+        calc.setA(-6);
+        calc.setB(-8);
+        calc.setOperation("-");
+        System.out.println(calc.toString());
+
+        calc.setOperation("abscs");
+        System.out.println(calc.toString());
+
+    }
+
+}
